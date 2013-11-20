dbcc8601512e31763139a61c8aa2e69fad54d74a
arek <arek.stryjski@gmail.com>
2013-11-20 12:23:40 +0000

    wip

--- local.js
+++ local.js
@@ -1,9 +1,9 @@
 (function ( $ ) {
+
 	$(document.links).filter(function() {
 	    return this.hostname != window.location.hostname;
 	}).attr('target', '_blank');
 
-	$('table').filter(function() {
-	    return this.hostname != window.location.hostname;
-	}).attr('class', 'table table-hover');
+	$('table').attr('class', 'table table-hover');
+
 }( jQuery ));
