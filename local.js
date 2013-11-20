7d9228f45060bf25c677c82c36f03341b3837b18
arek <arek.stryjski@gmail.com>
2013-11-20 12:05:06 +0000

    tabele

--- local.js
+++ local.js
@@ -2,4 +2,8 @@
 	$(document.links).filter(function() {
 	    return this.hostname != window.location.hostname;
 	}).attr('target', '_blank');
+
+	$('table').filter(function() {
+	    return this.hostname != window.location.hostname;
+	}).attr('class', 'table table-hover');
 }( jQuery ));
